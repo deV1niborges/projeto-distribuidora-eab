e2b0665cdcfceae02624c14fc3b6582c0e49fd4a
Vinicius Borges <138510552+deV1niborges@users.noreply.github.com>
2025-05-13 02:05:53 -0300

    criando servidor pra ve os paths

---
+++ server.js
@@ -0,0 +1,42 @@
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+const express = require("express");
+const path = require("path");
+
+const PORT = 8000;
+
+// Configura o LiveReload
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "src"));
+
+// Middleware para LiveReload
+const app = express();
+app.use(connectLivereload());
+
+// Servir arquivos estáticos
+app.use(express.static(__dirname));
+
+// Redireciona URLs amigáveis para os arquivos corretos
+app.get("/produtos", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/pages/produtos.html"));
+});
+
+app.get("/contato", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/pages/contato.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+// Atualiza o navegador automaticamente
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});

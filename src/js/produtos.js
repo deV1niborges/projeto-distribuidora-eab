1c1b31d9c40fb097e856f21a3c4296c29db1a165
Vinicius Borges <138510552+deV1niborges@users.noreply.github.com>
2025-05-17 23:58:48 -0300

    media query configurado e algumas alteracoes no script

--- src/js/produtos.js
+++ src/js/produtos.js
@@ -1,12 +1,29 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.location.hash) {
+    return;
+  }
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+  if (window.innerWidth <= 768) {
+    document
+      .querySelectorAll(".hidden")
+      .forEach((el) => el.classList.remove("hidden"));
+    return;
+  }
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.2,
+    }
+  );
+
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((el) => observer.observe(el));
+});
